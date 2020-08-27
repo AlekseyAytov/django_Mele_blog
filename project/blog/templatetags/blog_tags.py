9c7a4777f136afdf3b3aa123f784e6695c6a24b1
AlekseyAytov <a.aytov@inbox.ru>
2020-08-27 12:53:58 +0300

    27.08.2020

--- project/blog/templatetags/blog_tags.py
+++ project/blog/templatetags/blog_tags.py
@@ -6,3 +6,26 @@ register = template.Library()
 @register.simple_tag
 def total_posts():
     return Post.published.count()
+
+
+# Inclusion tags have to return a dictionary of values, 
+# which is used as the context to render the specified template.
+@register.inclusion_tag('latest_posts.html')
+def show_latest_posts(count=5):
+    latest_posts = Post.published.order_by('-publish')[:count]
+    return {'latest_posts': latest_posts}
+
+
+from django.db.models import Count
+
+@register.simple_tag
+def get_most_commented_posts(count=5):
+    return Post.published.annotate(total_comments=Count('comments')).order_by('-total_comments')[:count]
+
+
+from django.utils.safestring import mark_safe
+import markdown
+
+@register.filter(name='markdown')
+def markdown_format(text):
+    return mark_safe(markdown.markdown(text))
